4489b84ed7714593c82df5651531f3783f6614b4
Nakahara-alpha <abdulgafarolatunde2345@gmail.com>
2026-01-10 14:13:50 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,30 @@
+function updateTime() {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const time = new Date();
+  const hours = time.getHours();
+  const mins = time.getMinutes();
+  const meridian = hours >= 12 ? "pm" : "am";
+  const hoursDisplay = hours % 12 || 12;
+  const minsDisplay = mins < 10 ? "0" + mins : mins;
+  const timeText = `${
+    days[time.getDay()]
+  } ${hoursDisplay}:${minsDisplay} ${meridian}`;
+  const timeElement = document.querySelector(".time-text");
+  if (timeElement) {
+    timeElement.textContent = timeText;
+  }
+}
+
+updateTime();
+setInterval(updateTime, 60000);
+
+const fullscreenBtn = document.querySelector(".fullscreen-btn");
+if (fullscreenBtn) {
+  fullscreenBtn.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  });
+}
